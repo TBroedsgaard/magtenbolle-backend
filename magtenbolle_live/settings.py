768897dcf088104d3a8a0a114d74ee336f388e0c
Troels Brødsgaard <troels@broedsgaard.com>
2013-10-28 11:40:53 +0100

    updated settings.py, moved secrets into secret_settings.py

--- magtenbolle_live/settings.py
+++ magtenbolle_live/settings.py
@@ -1,25 +1,17 @@
 # Django settings for magtenbolle_live project.
 
+from secret_settings import *
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Troels Broedsgaard', 'troels@broedsgaard.com'),
 )
 
-MANAGERS = ADMINS
+# databases imported from secret_settings.py
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
+MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -29,7 +21,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Copenhagen'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -83,7 +75,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '1k@3q3^_7ci=vsruz9-u*7m9-07y^p!omww_$+-(x570-%%y$+'
+# imported from secret_settings.py
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
