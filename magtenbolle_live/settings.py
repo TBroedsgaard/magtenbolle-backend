33220dcc050b297e2f41e0a3d5a8ac9606b86b42
Troels Brødsgaard <troels@broedsgaard.com>
2013-10-28 08:25:13 -0400

    Fixed absolute urls to local files

--- magtenbolle_live/settings.py
+++ magtenbolle_live/settings.py
@@ -103,8 +103,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/troels/projects/django-troels/magtenbolle-backend/templates",
-    "/home/troels/projects/django-troels/magtenbolle-backend/mtb/templates",
+    "/home/arbrdk/magtenbolle_live/templates",
+    "/home/arbrdk/magtenbolle_live/mtb/templates",
 )
 
 INSTALLED_APPS = (
