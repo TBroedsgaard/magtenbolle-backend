5414f9811910a1a528be653400dad011e065c93e
Troels Brødsgaard <troels@broedsgaard.com>
2013-10-30 12:46:23 +0100

    Added ApiKeyAuthentication to CustomerResource

--- mtb/api.py
+++ mtb/api.py
@@ -1,8 +1,10 @@
-from tastypie.resources import ModelResource
 from mtb.models import Customer
+from tastypie.resources import ModelResource
+from tastypie.authentication import ApiKeyAuthentication
 
 
 class CustomerResource(ModelResource):
     class Meta:
         queryset = Customer.objects.all()
         resource_name = "customer"
+        authentication = ApiKeyAuthentication()
