f37ee97ebd3375f62b5d51c9e04748fa8d734376
Troels Brødsgaard <troels@broedsgaard.com>
2013-10-31 12:49:47 +0100

    Added DjangoAuthorization to mtb api

--- mtb/api.py
+++ mtb/api.py
@@ -1,6 +1,7 @@
 from mtb.models import Customer
 from tastypie.resources import ModelResource
 from tastypie.authentication import ApiKeyAuthentication
+from tastypie.authorization import DjangoAuthorization
 
 
 class CustomerResource(ModelResource):
@@ -8,3 +9,4 @@ class CustomerResource(ModelResource):
         queryset = Customer.objects.all()
         resource_name = "customer"
         authentication = ApiKeyAuthentication()
+        authorization = DjangoAuthorization()
